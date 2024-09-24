ff6fdabcd974dbb9164ffb906ed3b3a486dd76bc
Pranav Neti <pranav.neti108@gmail.com>
2024-09-24 16:19:45 -0400

    Implemented map reader to main file

--- Testing.py
+++ Testing.py
@@ -4,38 +4,93 @@ import pygame
 # Initialize Pygame
 pygame.init()
 
-# Set screen dimensions
-screen_width = 480
-screen_height = 480
-screen = pygame.display.set_mode((screen_width, screen_height))
+points = []
 
-# Set grid dimensions
-grid_size = 6
-cell_size = screen_width / grid_size
-
-# Colors
 black = (0, 0, 0)
 white = (255, 255, 255)
-blue = (0, 0, 255)
-red = (255, 0, 0)
-yellow = (255, 255, 0)
-pink = (255, 105, 180)
-green = (0, 255, 0)
-brown = (150, 75, 0)
-colors = [blue, red, yellow, pink, green, brown]
 
-# 2d Matricies
-colorGrid = [[blue, black, black, black, black, blue], 
-            [red, black, black, black, black, red], 
-            [yellow, black, black, black, black, yellow], 
-            [pink, black, black, black, black, pink], 
-            [green, black, black, black, black, green], 
-            [brown, black, black, black, black, brown]]
+xSize = 0
+ySize = 0
+circRad = 0
+cellSize = 0
 
-typeGrid = [[0] * grid_size] * grid_size
+f = open("map1.txt", "r")
+for x in f:
+    if(x[0:1] == "g"):
+        x = x[2:]
+        space = x.find(" ")
+        xSize = int(x[0:space])
+        x = x[space+1:]
+        space = x.find(" ")
+        ySize = int(x[0:space])
+        continue
+    if(x[0:1] == "s"):
+        x = x[2:]
+        space = x.find(" ")
+        circRad = int(x[0:space])
+        x = x[space+1:]
+        space = x.find(" ")
+        cellSize = int(x[0:space])
+        continue
 
-# Circle
-circRad = 40
+    space = x.find(" ")
+    red = int(x[0:space])
+    x = x[space+1:]
+    space = x.find(" ")
+    green = int(x[0:space])
+    x = x[space+1:]
+    space = x.find(" ")
+    blue = int(x[0:space])
+    x = x[space+3:]
+    space = x.find(" ")
+    firstXPos = int(x[0:space])
+    x = x[space+1:]
+    space = x.find(" ")
+    firstYPos = int(x[0:space])
+    points.append((firstXPos, firstYPos, (red, green, blue)))
+    x = x[space+3:]
+    space = x.find(" ")
+    secondXPos = int(x[0:space])
+    x = x[space+1:]
+    secondYPos = int(x[0:])
+    points.append((secondXPos, secondYPos, (red, green, blue)))
+    
+
+screen_width = cellSize * xSize
+screen_height = cellSize * ySize
+screen = pygame.display.set_mode((screen_width, screen_height))
+screen.fill(black)
+
+
+typeGrid = [[]]
+colorGrid = [[]]
+for i in range(xSize):
+    typeGrid.append([])
+    colorGrid.append([])
+    for j in range(ySize):
+        typeGrid[i].append(0)
+        colorGrid[i].append(black)
+
+for i in points:
+    typeGrid[i[0]][i[1]] = 2
+    colorGrid[i[0]][i[1]] = i[2]
+
+def pixelPos(num):
+    return num * cellSize + 40
+
+def draw():
+    for row in range(ySize):
+        for col in range(xSize):
+            x = col * cellSize
+            y = row * cellSize
+            if(typeGrid[row][col] == 2):
+                pygame.draw.circle(screen, colorGrid[row][col], (pixelPos(row), pixelPos(col)), 40 - 4)
+            pygame.draw.rect(screen, white, (x, y, cellSize, cellSize), 1)
+
+draw()
+
+pygame.display.flip()
+
 curCelX = 0
 curCelY = 0
 pastCelX = 0
@@ -43,52 +98,15 @@ pastCelY = 0
 pastPastCelX = 0
 pastPastCelY = 0
 
-# Clear the screen
-screen.fill(black)
-
-def pixelPos(num):
-    return num * cell_size + circRad
-
-# Draw the grid
-for row in range(grid_size):
-    typeGrid[row][0] = 2
-    typeGrid[row][-1] = 2
-    for col in range(grid_size):
-        x = col * cell_size
-        y = row * cell_size
-        if(typeGrid[row][col] == 2):
-            pygame.draw.circle(screen, colorGrid[row][col], (pixelPos(row), pixelPos(col)), circRad - 4)
-        pygame.draw.rect(screen, white, (x, y, cell_size, cell_size), 1)
-
-pygame.display.flip()
-
-print(colorGrid)
-
 inputs = (curCelX, curCelY, pastCelX, pastCelY, pastPastCelX, pastPastCelY, colorGrid[curCelX][curCelY], False)
 
-# startPosX = cell_size / 2
-# startPosY = cell_size / 2
-# curPosX = startPosX
-# curPosY = startPosY
-# pastPosX = curPosX
-# pastPosY = curPosY
-# pastPastPosX = curPosX
-# pastPastPosY = curPosY
-
 def input(curCelX, curCelY, pastCelX, pastCelY, pastPastCelX, pastPastCelY):
-    # pastPastCelX = pastPastCelX
-    # pastPastCelY = pastPastCelY
-    # pastCelX = pastCelX
-    # pastCelY = pastCelY
-    # curCelX = curCelX
-    # curCelY = curCelY
-
     mouseY = pygame.mouse.get_pos()[1]
     mouseX = pygame.mouse.get_pos()[0]
     mousePressed = pygame.mouse.get_pressed()[0]
 
-    futurecurCelX = (int) (mouseX / cell_size)
-    futurecurCelY = (int) (mouseY / cell_size)
+    futurecurCelX = (int) (mouseX / cellSize)
+    futurecurCelY = (int) (mouseY / cellSize)
 
     if( (pastPastCelX != pastCelX or pastPastCelY != pastCelY) and (pastCelX != curCelX or pastCelY != curCelY)):
         pastPastCelX = pastCelX
@@ -98,8 +116,8 @@ def input(curCelX, curCelY, pastCelX, pastCelY, pastPastCelX, pastPastCelY):
         pastCelX = curCelX
         pastCelY = curCelY
 
-    curCelX = (int) (mouseX / cell_size)
-    curCelY = (int) (mouseY / cell_size)
+    curCelX = (int) (mouseX / cellSize)
+    curCelY = (int) (mouseY / cellSize)
     if(colorGrid[curCelX][curCelY] != black):
         color = colorGrid[curCelX][curCelY]
     else:
@@ -124,7 +142,7 @@ def input(curCelX, curCelY, pastCelX, pastCelY, pastPastCelX, pastPastCelY):
 lastTurn = [False, False, False, False, False, False, False, False]
     
 def DrawBoard(curCelX, curCelY, pastCelX, pastCelY, pastPastCelX, pastPastCelY, color, bleh):
-    print(str(curCelX) + " " + str(curCelY) + " " + str(pastCelX) + " " + str(pastCelY) + " " + str([pastPastCelX]) + " " + str(pastPastCelY))
+    print(str(curCelX) + " " + str(curCelY) + " " + str(pastCelX) + " " + str(pastCelY) + " " + str([pastPastCelX]) + " " + str(pastPastCelY) + " " + str(color))
     if((typeGrid[curCelX][curCelY] != 2) and (typeGrid[pastCelX][pastCelY] != 2)):
         if(curCelX == pastCelX and curCelY > pastCelY and pastPastCelX < pastCelX and lastTurn[0] == False):
             pygame.draw.rect(screen, black, (pixelPos(pastCelX) - circRad, pixelPos(pastCelY) - circRad / 2, circRad * 2, circRad), 0)
@@ -246,19 +264,6 @@ first = True
 # Game loop
 running = True
 while running:
-    # mouseX = pygame.mouse.get_pos()[0]
-    # mouseY = pygame.mouse.get_pos()[1]
-    # mousePressed = pygame.mouse.get_pressed()[0]
-    
-    
-    # if(curPosX != pastPosX or curPosY != pastPosY):
-    #     pastPastPosX = pastPosX
-    #     pastPastPosY = pastPosY
-    
-
-    # pastPosX = curPosX
-    # pastPosY = curPosY
-    
     pygame.time.delay(20)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -277,135 +282,9 @@ while running:
     else:
         inputs = last
         continue
-    # if(mousePressed and (abs(mouseX - curPosX) > 40 or abs(mouseY - curPosY) > 40)):
-    #     if(mouseX > curPosX + 40 and curPosX < 440):
-    #         curPosX += 80
-
-    #     elif(mouseY < curPosY - 40 and curPosY > 40):
-    #         curPosY -= 80
-
-    #     elif(mouseX < curPosX - 40 and curPosX > 40):
-    #         curPosX -= 80
-
-    #     elif(mouseY > curPosY + 40 and curPosY < 440):
-    #         curPosY += 80
-    
-
 
     if(keys[pygame.K_q]):
         break
-    
-
-    # pygame.draw.circle(screen, black, (pastPosX, pastPosY), circRad)
-
-    # draw right to down
-    # if((curPosX != startPosX or curPosY != startPosY) and curPosX == pastPosX and curPosY > pastPosY and pastPastPosX < pastPosX and (pastPosX != startPosX or pastPosY != startPosY) and lastTurn[0] == False):
-    #     pygame.draw.rect(screen, black, (pastPosX - circRad, pastPosY - circRad / 2, circRad * 2, circRad), 0)
-    #     pygame.draw.circle(screen, blue, (pastPosX, pastPosY), circRad / 2)
-    #     pygame.draw.rect(screen, blue, (pastPosX - circRad, pastPosY - circRad / 2, circRad, circRad))
-    #     pygame.draw.rect(screen, blue, (pastPosX - circRad / 2, pastPosY, circRad, circRad))
-    #     pygame.draw.rect(screen, blue, (curPosX - circRad / 2, curPosY - circRad, circRad, circRad * 2), 0)
-    #     for i in range(len(lastTurn)):
-    #         lastTurn[i] = False
-    #     lastTurn[0] = True
-    
-    # # draw right to up
-    # elif((curPosX != startPosX or curPosY != startPosY) and curPosX == pastPosX and curPosY < pastPosY and pastPastPosX < pastPosX and (pastPosX != startPosX or pastPosY != startPosY) and lastTurn[1] == False):
-    #     pygame.draw.rect(screen, black, (pastPosX - circRad, pastPosY - circRad / 2, circRad * 2, circRad), 0)
-    #     pygame.draw.circle(screen, blue, (pastPosX, pastPosY), circRad / 2)
-    #     pygame.draw.rect(screen, blue, (pastPosX - circRad, pastPosY - circRad / 2, circRad, circRad))
-    #     pygame.draw.rect(screen, blue, (pastPosX - circRad / 2, pastPosY - circRad, circRad, circRad))
-    #     pygame.draw.rect(screen, blue, (curPosX - circRad / 2, curPosY - circRad, circRad, circRad * 2), 0)
-    #     for i in range(len(lastTurn)):
-    #         lastTurn[i] = False
-    #     lastTurn[1] = True
-
-    # # draw up to right
-    # elif((curPosX != startPosX or curPosY != startPosY) and curPosX > pastPosX and curPosY == pastPosY and pastPastPosY > pastPosY and (pastPosX != startPosX or pastPosY != startPosY) and lastTurn[2] == False):
-    #     pygame.draw.rect(screen, black, (pastPosX - circRad / 2, pastPosY - circRad, circRad, circRad * 2), 0)
-    #     pygame.draw.circle(screen, blue, (pastPosX, pastPosY), circRad / 2)
-    #     pygame.draw.rect(screen, blue, (pastPosX, pastPosY - circRad / 2, circRad, circRad))
-    #     pygame.draw.rect(screen, blue, (pastPosX - circRad / 2, pastPosY, circRad, circRad))
-    #     pygame.draw.rect(screen, blue, (curPosX - circRad, curPosY - circRad / 2, circRad * 2, circRad), 0)
-    #     for i in range(len(lastTurn)):
-    #         lastTurn[i] = False
-    #     lastTurn[2] = True
-
-    # # draw up to left
-    # elif((curPosX != startPosX or curPosY != startPosY) and curPosX < pastPosX and curPosY == pastPosY and pastPastPosY > pastPosY and (pastPosX != startPosX or pastPosY != startPosY) and lastTurn[3] == False):
-    #     pygame.draw.rect(screen, black, (pastPosX - circRad / 2, pastPosY - circRad, circRad, circRad * 2), 0)
-    #     pygame.draw.circle(screen, blue, (pastPosX, pastPosY), circRad / 2)
-    #     pygame.draw.rect(screen, blue, (pastPosX - circRad, pastPosY - circRad / 2, circRad, circRad))
-    #     pygame.draw.rect(screen, blue, (pastPosX - circRad / 2, pastPosY, circRad, circRad))
-    #     pygame.draw.rect(screen, blue, (curPosX - circRad, curPosY - circRad / 2, circRad * 2, circRad), 0)
-    #     for i in range(len(lastTurn)):
-    #         lastTurn[i] = False
-    #     lastTurn[3] = True
-
-    # # draw left to down
-    # elif((curPosX != startPosX or curPosY != startPosY) and curPosX == pastPosX and curPosY > pastPosY and pastPastPosX > pastPosX and (pastPosX != startPosX or pastPosY != startPosY) and lastTurn[4] == False):
-    #     pygame.draw.rect(screen, black, (pastPosX - circRad, pastPosY - circRad / 2, circRad * 2, circRad), 0)
-    #     pygame.draw.circle(screen, blue, (pastPosX, pastPosY), circRad / 2)
-    #     pygame.draw.rect(screen, blue, (pastPosX, pastPosY - circRad / 2, circRad, circRad))
-    #     pygame.draw.rect(screen, blue, (pastPosX - circRad / 2, pastPosY, circRad, circRad))
-    #     pygame.draw.rect(screen, blue, (curPosX - circRad / 2, curPosY - circRad, circRad, circRad * 2), 0)
-    #     for i in range(len(lastTurn)):
-    #         lastTurn[i] = False
-    #     lastTurn[4] = True
-
-    # # draw left to up
-    # elif((curPosX != startPosX or curPosY != startPosY) and curPosX == pastPosX and curPosY < pastPosY and pastPastPosX > pastPosX and (pastPosX != startPosX or pastPosY != startPosY) and lastTurn[5] == False):
-    #     pygame.draw.rect(screen, black, (pastPosX - circRad, pastPosY - circRad / 2, circRad * 2, circRad), 0)
-    #     pygame.draw.circle(screen, blue, (pastPosX, pastPosY), circRad / 2)
-    #     pygame.draw.rect(screen, blue, (pastPosX, pastPosY - circRad / 2, circRad, circRad))
-    #     pygame.draw.rect(screen, blue, (pastPosX - circRad / 2, pastPosY - circRad, circRad, circRad))
-    #     pygame.draw.rect(screen, blue, (curPosX - circRad / 2, curPosY - circRad, circRad, circRad * 2), 0)
-    #     for i in range(len(lastTurn)):
-    #         lastTurn[i] = False
-    #     lastTurn[5] = True
-    
-    # # draw down to right
-    # elif((curPosX != startPosX or curPosY != startPosY) and curPosX > pastPosX and curPosY == pastPosY and pastPastPosY < pastPosY and (pastPosX != startPosX or pastPosY != startPosY) and lastTurn[6] == False):
-    #     pygame.draw.rect(screen, black, (pastPosX - circRad / 2, pastPosY - circRad, circRad, circRad * 2), 0)
-    #     pygame.draw.circle(screen, blue, (pastPosX, pastPosY), circRad / 2)
-    #     pygame.draw.rect(screen, blue, (pastPosX, pastPosY - circRad / 2, circRad, circRad))
-    #     pygame.draw.rect(screen, blue, (pastPosX - circRad / 2, pastPosY - circRad, circRad, circRad))
-    #     pygame.draw.rect(screen, blue, (curPosX - circRad, curPosY - circRad / 2, circRad * 2, circRad), 0)
-    #     for i in range(len(lastTurn)):
-    #         lastTurn[i] = False
-    #     lastTurn[6] = True
-
-    # # draw down to left
-    # elif((curPosX != startPosX or curPosY != startPosY) and curPosX < pastPosX and curPosY == pastPosY and pastPastPosY < pastPosY and (pastPosX != startPosX or pastPosY != startPosY) and lastTurn[7] == False):
-    #     pygame.draw.rect(screen, black, (pastPosX - circRad / 2, pastPosY - circRad, circRad, circRad * 2), 0)
-    #     pygame.draw.circle(screen, blue, (pastPosX, pastPosY), circRad / 2)
-    #     pygame.draw.rect(screen, blue, (pastPosX - circRad, pastPosY - circRad / 2, circRad, circRad))
-    #     pygame.draw.rect(screen, blue, (pastPosX - circRad / 2, pastPosY - circRad, circRad, circRad))
-    #     pygame.draw.rect(screen, blue, (curPosX - circRad, curPosY - circRad / 2, circRad * 2, circRad), 0)
-    #     for i in range(len(lastTurn)):
-    #         lastTurn[i] = False
-    #     lastTurn[7] = True
-    
-    
-
-    # # draw normals
-    # elif((curPosX != startPosX or curPosY != startPosY) and curPosX != pastPosX):
-    #     pygame.draw.rect(screen, blue, (curPosX - circRad, curPosY - circRad / 2, circRad * 2, circRad), 0)
-    # elif((curPosX != startPosX or curPosY != startPosY) and curPosY != pastPosY):
-    #     pygame.draw.rect(screen, blue, (curPosX - circRad / 2, curPosY - circRad, circRad, circRad * 2), 0)
-
-    
-
-
-    # pygame.draw.arc(screen, blue, (180, 20, 40, 40), 0, math.pi / 2, 20)
-    # pygame.draw.circle(screen, black, (180, 60), 20)
-
-    # pygame.draw.rect(screen, black, (170, 40, 10, 10))
-    # pygame.draw.rect(screen, black, (190, 60, 10, 10))
-    
-    
-
-
 
     # Update the display
     pygame.display.flip()
--- readingFile.py
+++ readingFile.py
@@ -1,17 +1,36 @@
-possibleColors = []
+import pygame
+
+pygame.init()
+
 points = []
+
+black = (0, 0, 0)
+white = (255, 255, 255)
 
 xSize = 0
 ySize = 0
+circRad = 0
+cellSize = 0
 
-f = open("map2.txt", "r")
+f = open("map1.txt", "r")
 for x in f:
     if(x[0:1] == "g"):
         x = x[2:]
-        xSize = int(x[0:1])
-        x = x[2:]
-        ySize = int(x[0:1])
+        space = x.find(" ")
+        xSize = int(x[0:space])
+        x = x[space+1:]
+        space = x.find(" ")
+        ySize = int(x[0:space])
         continue
+    if(x[0:1] == "s"):
+        x = x[2:]
+        space = x.find(" ")
+        circRad = int(x[0:space])
+        x = x[space+1:]
+        space = x.find(" ")
+        cellSize = int(x[0:space])
+        continue
+
     space = x.find(" ")
     red = int(x[0:space])
     x = x[space+1:]
@@ -20,7 +39,6 @@ for x in f:
     x = x[space+1:]
     space = x.find(" ")
     blue = int(x[0:space])
-    possibleColors.append((red, green, blue))
     x = x[space+3:]
     space = x.find(" ")
     firstXPos = int(x[0:space])
@@ -36,10 +54,50 @@ for x in f:
     points.append((secondXPos, secondYPos, (red, green, blue)))
     
 
-print(xSize)
-print(ySize)
+screen_width = cellSize * xSize
+screen_height = cellSize * ySize
+screen = pygame.display.set_mode((screen_width, screen_height))
 
+typeGrid = [[]]
+colorGrid = [[]]
+for i in range(xSize):
+    typeGrid.append([])
+    colorGrid.append([])
+    for j in range(ySize):
+        typeGrid[i].append(0)
+        colorGrid[i].append([black])
 
-print(possibleColors)
+for i in points:
+    typeGrid[i[0]][i[1]] = 2
+    colorGrid[i[0]][i[1]] = i[2]
 
 print(points)
+
+def pixelPos(num):
+    return num * cellSize + 40
+
+def draw():
+    for row in range(ySize):
+        for col in range(xSize):
+            x = col * cellSize
+            y = row * cellSize
+            if(typeGrid[row][col] == 2):
+                pygame.draw.circle(screen, colorGrid[row][col], (pixelPos(row), pixelPos(col)), 40 - 4)
+            pygame.draw.rect(screen, white, (x, y, cellSize, cellSize), 1)
+
+draw()
+
+pygame.display.flip()
+
+running = True
+
+while(running):
+
+    pygame.time.delay(20)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+
+pygame.quit()
